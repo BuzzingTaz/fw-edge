3e0acb0ecb8929df8002aa403f445b00d7edb5e9
Vishnu-Muralidhar <vishnumuralidhar.mk@gmail.com>
2026-04-16 10:28:33 +0530

    refactor

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -61,16 +61,18 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 
 		ctx, cancel := context.WithTimeout(context.Background(), 300*time.Second)
 		defer cancel()
-		// NOTE: Only creating rtp streaming for now, change to decoded frames
+		// NOTE: Only creating rtp streaming for now, change to decoded frames (gRPC)
+		//sending to compute server (gRPC)
 		computeVideoStreamer, err := computeStreamClient.StreamVideo(ctx)
 		if err != nil {
 			log.Println("Failed to create rpc from client: %v", err)
 			return
 		}
 
-		go ReadInferenceData(computeVideoStreamer)
+		go ReadInferenceData(computeVideoStreamer) // in another thread
 
 		for {
+			// recieve video frames from client via websocket, marshal them into rtp packets and send to compute server via gRPC stream
 			err = tempConn.ReadJSON(&message)
 			if err != nil {
 				fmt.Println("WebSocket read error:", err)
@@ -81,6 +83,7 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("Data length:", len(message.Payload))
 			fmt.Println(" from user:", userID)
 
+			// convert rtp to raw bytes to send to compute server via gRPC
 			rawBytes, err := message.Marshal()
 			if err != nil {
 				log.Println("Error marshaling RTP packet:", err)
@@ -99,6 +102,7 @@ func SendDecodedFrameData(stream grpc.BidiStreamingClient[pb.DecodedFrame, pb.In
 
 }
 
+// read the inference result from the compute server and forward it to the client via the websocket connection. This will run in a separate goroutine so that it can listen for inference results while still allowing the main goroutine to listen for incoming video frames from the client.
 func ReadInferenceData(stream grpc.BidiStreamingClient[pb.RTPPacket, pb.InferenceResult]) {
 	for {
 		inferenceData, err := stream.Recv()
@@ -116,6 +120,7 @@ func ReadInferenceData(stream grpc.BidiStreamingClient[pb.RTPPacket, pb.Inferenc
 			inferenceData.Timestamp, inferenceData.ProcessingStatus, len(inferenceData.Detections))
 
 		if tempConn != nil {
+			//write back to client via websocket
 			err = tempConn.WriteJSON(inferenceData)
 			if err != nil {
 				log.Printf("Error sending inference data to client: %v", err)
@@ -132,21 +137,7 @@ func ReadInferenceData(stream grpc.BidiStreamingClient[pb.RTPPacket, pb.Inferenc
 func main() {
 	flag.Parse()
 
-	ffmpeg := exec.Command("ffmpeg", "-i", "pipe:0", "-pix_fmt", "bgr24", "-s", strconv.Itoa(frameX)+"x"+strconv.Itoa(frameY), "-f", "rawvideo", "pipe:1") //nolint
-	// ffmpegIn, _ := ffmpeg.StdinPipe()
-	// ffmpegOut, _ := ffmpeg.StdoutPipe()
-	ffmpegErr, _ := ffmpeg.StderrPipe()
-
-	if err := ffmpeg.Start(); err != nil {
-		panic(err)
-	}
-	go func() {
-		scanner := bufio.NewScanner(ffmpegErr)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-		}
-	}()
-
+	//Connect to Compute Server (gRPC)
 	computeUnitConn, err := grpc.NewClient(*computeAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect to compute: %v", err)
@@ -154,9 +145,11 @@ func main() {
 	defer computeUnitConn.Close()
 	computeStreamClient = pb.NewComputeStreamClient(computeUnitConn)
 
+	// starting the Web Socket server to send inference results back to the client in a seperate thread.
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Println("Bello!") })
-	http.HandleFunc("/ws/{userID}", wsHandler)
+	http.HandleFunc("/ws/{userID}", wsHandler) //when client connects to ws, it will call the wsHandler function which will upgrade the connection to a websocket and start listening for messages from the client (e.g., frames to process) and also send inference results back to the client when they are received from the compute server. The userID in the path can be used to identify different clients if needed.
 	fmt.Println("Server starting on :9998")
+	
 	go func() {
 		if err := http.ListenAndServe(":9998", nil); err != nil {
 			fmt.Println("Failed to start server:", err)
@@ -165,7 +158,8 @@ func main() {
 
 	// frameConsumer := consumer.NewConsumer(clientID, nats URL, subscribeSubject, queueGroup)
 	// go frameConsumer.StartConsuming()
-
+	
+	//TODO: Initialize the scheduler with the appropriate parameters (e.g., list of compute nodes, scheduling algorithm, etc.)
 	frameScheduler := scheduler.NewScheduler(nil, nil, []string{"node1", "node2", "node3"})
 	fmt.Println(frameScheduler)
 	// go frameScheduler.Run()
